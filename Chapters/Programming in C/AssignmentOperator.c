69fe3e03d32e3f5ee386035bacffcefb7e4a9ca3
dev-ajay-kr <117085146+dev-ajay-kr@users.noreply.github.com>
2022-12-16 15:35:04 +0530

    added more operator

---
+++ Chapters/Programming in C/AssignmentOperator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a = 5, c;
+    c = a;
+    printf("c= %d \n", c);
+    c += a;
+    printf("c=+ %d \n ", c);
+    c -= a;
+    printf("c=- %d \n ", c);    
+    c *= a;
+    printf("c=* %d \n ", c);
+    c /= a;
+    printf("c=/ %d \n ", c);
+    c %= a;
+
+    printf("c=%` %d \n ", c);
+}
---
+++ Chapters/Programming in C/incrementOperators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 10, b = 100;
+    float c = 10.5, d = 100.5;
+    printf("++a %d \n", ++a);
+    printf("--b= %d\n", --b);
+   
+    printf("++c=%d \n", ++c);
+    printf("--d= %d \n", --d);
+}
